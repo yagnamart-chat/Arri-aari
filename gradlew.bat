09a01c2eb97cd768c0456b70a7b900ff5afe202e
yagnamart-chat <jhasaurav19206@gmail.com>
2026-04-25 13:00:12 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -2,4 +2,9 @@
 set DIR=%~dp0
 set JAR=%DIR%gradle\wrapper\gradle-wrapper.jar
 
-java -jar %JAR% %*
+if not exist "%JAR%" (
+  echo ERROR: gradle-wrapper.jar not found!
+  exit /b 1
+)
+
+java -jar "%JAR%" %*
